e2c0ed1cc04221b3fda618db60aa4c90aa50c9d7
Mario Morales <marioalejandrom@gmail.com>
2019-02-26 14:20:18 -0600

    Added admin listing and tests

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,44 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+from rest_framework import status
+
+User = get_user_model()
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = User.objects.create_superuser(
+            email='admin@test.net',
+            password='test1234'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = User.objects.create_user(
+            email='test@test.net',
+            password='password1234',
+            first_name='Test',
+            last_name='User'
+        )
+
+    def test_users_listed(self):
+        """Test that the users are listed on the user page"""
+        url = reverse('admin:core_user_changelist')
+        response = self.client.get(url)
+        self.assertContains(response, self.user.first_name)
+        self.assertContains(response, self.user.last_name)
+        self.assertContains(response, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_create_page(self):
+        """"Test that the create user page works"""
+        url = reverse('admin:core_user_add')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
