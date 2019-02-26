eb5f6c56a32359354eb8f00919b3215fe2683008
Mario Morales <marioalejandrom@gmail.com>
2019-02-25 18:52:06 -0600

    Added email validation for user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import validate_email
 from django.contrib.auth.models import AbstractBaseUser, \
     BaseUserManager, PermissionsMixin
 
@@ -7,9 +8,8 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
-        if not email:
-            raise ValueError('Users must have an email address')
-        user = self.model(email=email, **extra_fields)
+        validate_email(email)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -6,7 +7,8 @@ User = get_user_model()
 
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
-        """Test creating a new user with email as username successfully"""
+        """Test creating a new user with
+        email as username successfully"""
         email = 'test@test.net'
         password = 'test1234'
         user = User.objects.create_user(
@@ -15,3 +17,22 @@ class ModelTests(TestCase):
         )
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_create_user_with_invalid_email_failure(self):
+        """Test creating an user with invalid email raises error"""
+        email = 'test@googlecom'  # or empty
+        password = 'test1234'
+        with self.assertRaises(ValidationError):
+            User.objects.create_user(
+                email=email,
+                password=password
+            )
+
+    def test_new_user_email_normalized(self):
+        """Test that the email for new user is normalized"""
+        email = 'test@TEST.net'
+        user = User.objects.create_user(
+            email=email,
+            password='1234Password'
+        )
+        self.assertEqual(user.email, email.lower())
